87fbf07416d3a8c9cd84e96a0ebc808a1f590e68
Felice Serena <felice@serena-mueller.ch>
2017-06-17 01:05:57 +0200

    Binary write implemented.

--- src/writer.rs
+++ src/writer.rs
@@ -1,5 +1,8 @@
 use std::io::{ Write, Result, Error, ErrorKind };
 use std::string::ToString;
+
+use byteorder::{ BigEndian, LittleEndian, WriteBytesExt, ByteOrder };
+
 use ply::*;
 
 pub enum NewLine {
@@ -46,7 +49,7 @@ impl<P: ToElement<P>> Writer<P> {
     pub fn write_ply<T: Write>(&mut self, out: &mut T, ply: &Ply<P>) -> Result<usize> {
         let mut written = 0;
         written += try!(self.write_header(out, &ply.header));
-        written += try!(self.write_payload(out, &ply.payload, &ply.header.elements));
+        written += try!(self.write_payload(out, &ply.payload, &ply.header));
         out.flush().unwrap();
         Ok(written)
     }
@@ -157,22 +160,95 @@ impl<P: ToElement<P>> Writer<P> {
         }
     }
     ///// Payload
-    pub fn write_payload<T: Write>(&mut self, out: &mut T, payload: &Payload<P>, element_defs: &KeyMap<ElementDef>) -> Result<usize> {
+    pub fn write_payload<T: Write>(&mut self, out: &mut T, payload: &Payload<P>, header: &Header) -> Result<usize> {
         let mut written = 0;
+        let element_defs = &header.elements;
         for (k, element_list) in payload {
             let element_def = &element_defs[k];
-            for e in element_list {
+            written += try!(self.write_payload_of_element(out, element_list, element_def, header));
+        }
+        Ok(written)
+    }
+    pub fn write_payload_of_element<T: Write>(&mut self, out: &mut T, element_list: &Vec<P>, element_def: &ElementDef, header: &Header) -> Result<usize> {
+        let mut written = 0;
+        match header.encoding {
+            Encoding::Ascii => for e in element_list {
                 let raw_element = try!(e.to_element(element_def));
-                written += try!(self.write_line_payload_element(out, &raw_element));
+                written += try!(self.__write_ascii_element(out, &raw_element));
+            },
+            Encoding::BinaryBigEndian => for e in element_list {
+                let raw_element = try!(e.to_element(element_def));
+                written += try!(self.__write_binary_element::<T, BigEndian>(out, &raw_element, &element_def));
+            },
+            Encoding::BinaryLittleEndian => for e in element_list {
+                let raw_element = try!(e.to_element(element_def));
+                written += try!(self.__write_binary_element::<T, LittleEndian>(out, &raw_element, &element_def));
             }
         }
         Ok(written)
     }
-    pub fn write_line_payload_element<T: Write>(&mut self, out: &mut T, element: &DefaultElement) -> Result<usize> {
+    pub fn write_ascii_element<T: Write>(&self, out: &mut T, element: &P, element_def: &ElementDef) -> Result<usize> {
+        let raw_element = try!(element.to_element(element_def));
+        self.__write_ascii_element(out, &raw_element)
+    }
+    pub fn write_big_endian_element<T: Write> (&self, out: &mut T, element: &P, element_def: &ElementDef) -> Result<usize> {
+        let raw_element = try!(element.to_element(element_def));
+        self.__write_binary_element::<T, BigEndian>(out, &raw_element, element_def)
+    }
+    pub fn write_little_endian_element<T: Write> (&self, out: &mut T, element: &P, element_def: &ElementDef) -> Result<usize> {
+        let raw_element = try!(element.to_element(element_def));
+        self.__write_binary_element::<T, BigEndian>(out, &raw_element, element_def)
+    }
+
+    // private payload
+    fn __write_binary_element<T: Write, B: ByteOrder>(&self, out: &mut T, element: &DefaultElement, element_def: &ElementDef) -> Result<usize> {
+        let mut written = 0;
+        for (k, property) in element {
+            written += try!(self.__write_binary_property::<T, B>(out, property, &element_def.properties[k].data_type));
+        }
+        Ok(written)
+    }
+    fn __write_binary_property<T: Write, B: ByteOrder>(&self, out: &mut T, property: &Property, property_type: &PropertyType) -> Result<usize> {
+         let result: usize = match *property {
+            Property::Char(ref v) => {try!(out.write_i8(*v)); 1},
+            Property::UChar(ref v) => {try!(out.write_u8(*v)); 1},
+            Property::Short(ref v) => {try!(out.write_i16::<B>(*v)); 2},
+            Property::UShort(ref v) => {try!(out.write_u16::<B>(*v)); 2},
+            Property::Int(ref v) => {try!(out.write_i32::<B>(*v)); 4},
+            Property::UInt(ref v) => {try!(out.write_u32::<B>(*v)); 4},
+            Property::Float(ref v) => {try!(out.write_f32::<B>(*v)); 4},
+            Property::Double(ref v) => {try!(out.write_f64::<B>(*v)); 8},
+            Property::List(ref v) => {
+                let mut written = 0;
+                let index_type = match *property_type {
+                    PropertyType::List(ref i, _) => i,
+                    _ => return Err(Error::new(ErrorKind::InvalidInput, "Property definition must be of type List.")),
+                };
+                let vl = v.len();
+                written += match **index_type {
+                    PropertyType::Char => {try!(out.write_i8(vl as i8)); 1},
+                    PropertyType::UChar => {try!(out.write_u8(vl as u8)); 1}
+                    PropertyType::Short => {try!(out.write_i16::<B>(vl as i16)); 2},
+                    PropertyType::UShort => {try!(out.write_u16::<B>(vl as u16)); 2},
+                    PropertyType::Int => {try!(out.write_i32::<B>(vl as i32)); 4}
+                    PropertyType::UInt => {try!(out.write_u32::<B>(vl as u32)); 4},
+                    PropertyType::Float => return Err(Error::new(ErrorKind::InvalidInput, "List index must have integer type, Float found.")),
+                    PropertyType::Double => return Err(Error::new(ErrorKind::InvalidInput, "List index must have integer type, Double found.")),
+                    PropertyType::List(_,_) => return Err(Error::new(ErrorKind::InvalidInput, "List index must have integer type, List found.")),
+                };
+                for e in v {
+                    written += try!(self.__write_binary_property::<T, B>(out, &e, &*index_type));
+                }
+                written as usize
+            },
+        };
+        Ok(result)
+    }
+    fn __write_ascii_element<T: Write>(&self, out: &mut T, element: &DefaultElement) -> Result<usize> {
         let mut written = 0;
         let mut p_iter = element.iter();
         let (_name, prop_val) = p_iter.next().unwrap();
-        written += try!(self.write_payload_property(out, prop_val));
+        written += try!(self.write_ascii_property(out, prop_val));
         loop {
             written += try!(out.write(" ".as_bytes()));
             let n = p_iter.next();
@@ -180,12 +256,12 @@ impl<P: ToElement<P>> Writer<P> {
                 break;
             }
             let (_name, prop_val) = n.unwrap();
-            written += try!(self.write_payload_property(out, prop_val));
+            written += try!(self.write_ascii_property(out, prop_val));
         }
         written += try!(self.write_new_line(out));
         Ok(written)
     }
-    fn write_payload_property<T: Write>(&self, out: &mut T, data_element: &Property) -> Result<usize> {
+    fn write_ascii_property<T: Write>(&self, out: &mut T, data_element: &Property) -> Result<usize> {
          let result = match *data_element {
             Property::Char(ref v) => self.write_simple_value(v, out),
             Property::UChar(ref v) => self.write_simple_value(v, out),
@@ -200,7 +276,7 @@ impl<P: ToElement<P>> Writer<P> {
                 written += try!(out.write(&v.len().to_string().as_bytes()));
                 for e in v {
                     written += try!(out.write(" ".as_bytes()));
-                    written += try!(self.write_payload_property(out, &e));
+                    written += try!(self.write_ascii_property(out, &e));
                 }
                 Ok(written)
             },
